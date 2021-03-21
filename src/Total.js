9baccc0bfbdfeaffdba18679a2b306813f0c1f92
wicp <noneof@yourbusiness.com>
2021-03-21 16:14:48 +0000

    completed 1.4

--- src/Total.js
+++ src/Total.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const Total = (props) => (
-    <p>Number of exercises {props.total}</p>
-)
+const Total = (props) => {
+    const total = props.part.reduce((total,prop) => total+prop.exercises,0)
+    return (
+    <p>Number of exercises {total}</p>
+    )}
 export default Total
